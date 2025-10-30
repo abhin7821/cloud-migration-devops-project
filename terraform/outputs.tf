20628f27caf0fef0f182f914e4c60a5411dd0dbf
abhin7821 <abhin7821@gmail.com>
2025-10-31 00:34:19 +0530

    Define Terraform outputs for EKS and VPC
    Added outputs for EKS cluster and VPC details.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,29 @@
+####################################################
+# 📤 Terraform Outputs for Cloud Migration Project
+####################################################
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint URL"
+  value       = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
 
