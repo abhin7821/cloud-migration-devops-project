d3e217f640b14f1b469f0b4ca63258907970cd3c
abhin7821 <abhin7821@gmail.com>
2025-10-31 01:08:45 +0530

    Remove output variables from main.tf
    Removed output variables for EKS cluster and VPC.

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,23 +70,3 @@ module "eks" {
     Project = "Cloud-Migration-DevOps"
   }
 }
-
-####################################################
-# 📤 Outputs
-####################################################
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
-
