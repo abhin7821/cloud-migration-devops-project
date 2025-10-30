05cef7df366514e7cd52978db7814811f48d25be
abhin7821 <abhin7821@gmail.com>
2025-10-31 00:31:24 +0530

    Add Terraform configuration for EKS and VPC
    Define Terraform configuration for EKS and VPC infrastructure.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,92 @@
+####################################################
+# 🚀 Terraform EKS + VPC Infrastructure
+####################################################
+
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+####################################################
+# 🧱 VPC Module
+####################################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "cloud-migration-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Project = "Cloud-Migration-DevOps"
+  }
+}
+
+####################################################
+# ☸️ EKS Cluster Module
+####################################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "cloud-migration-eks"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+  enable_irsa                    = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  cluster_endpoint_private_access = false
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 2
+    }
+  }
+
+  tags = {
+    Project = "Cloud-Migration-DevOps"
+  }
+}
+
+####################################################
+# 📤 Outputs
+####################################################
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
 
