fcca127a1a96d5bf784a8b9541b9c66aba75707a
abhin7821 <abhin7821@gmail.com>
2025-10-31 00:33:25 +0530

    Define variables for Cloud Migration project
    Added variables for AWS infrastructure configuration including VPC, EKS cluster settings, and project tags.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,74 @@
+####################################################
+# 🚀 Variables for Cloud Migration DevOps Project
+####################################################
+
+# AWS region where all resources will be created
+variable "region" {
+  description = "AWS region for infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
+
+# VPC configuration
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+# EKS Cluster settings
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "cloud-migration-eks"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for EKS"
+  type        = string
+  default     = "1.29"
+}
+
+variable "node_instance_type" {
+  description = "Instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+# Project tags
+variable "project_name" {
+  description = "Project tag name"
+  type        = string
+  default     = "Cloud-Migration-DevOps"
+}
 
